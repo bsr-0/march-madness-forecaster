e6b0f30507a82406433ec6dfc8e53aeb5fdc92db
Ben Rosen <benrosen25@gmail.com>
2026-04-21 22:45:04 -0500

    upd

--- src/forecaster/__init__.py
+++
@@ -1,1 +0,0 @@
-"""State machine forecaster for March Madness predictions."""
--- src/validation/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data-integrity validation for the March Madness forecaster pipeline."""
-
-from src.validation.data_integrity import (  # noqa: F401
-    DataIntegrityValidator,
-    ValidationReport,
-)
